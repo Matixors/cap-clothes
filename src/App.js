3a694baba26b2c8e28181af649fba0e6584aa985
Matixors <mbernacki.developer@gmail.com>
2023-05-07 14:26:20 +0200

    splitted code into smaller components

--- src/App.js
+++ src/App.js
@@ -1,40 +1,7 @@
-function App() {
-  const categoories = [
-    {
-      id: 1,
-      title: 'Hats',
-    },
-    {
-      id: 2,
-      title: 'Jackets',
-    },
-    {
-      id: 3,
-      title: 'Sneakers',
-    },
-    {
-      id: 4,
-      title: 'Womens',
-    },
-    {
-      id: 5,
-      title: 'Mens',
-    },
-  ];
+import CategoryMenu from './components/category-menu/category-menu.component';
 
-  return (
-    <div className='categories-container'>
-      {categoories.map(({ title, id }) => (
-        <div className='category-container' key={id}>
-          <div className='background-image' />
-          <div className='category-body-container'>
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+function App() {
+  return <CategoryMenu />;
 }
 
 export default App;
